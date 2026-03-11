5cf89fab5f04b02e71c3a4a8bd6c774f367f5e1c
Ajay Singh Bisht <ajaybisht203@gmail.com>
2026-03-12 00:49:11 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Employees from "./pages/Employees"
 import Attendance from "./pages/Attendance"
 import Navbar from "./components/Navbar"
 import "./App.css"
 
 function App() {
+
   return (
-    <BrowserRouter basename="/HRMS-LITE">
+
+    <HashRouter>
+
       <Navbar />
+
       <Routes>
+
         <Route path="/" element={<Employees />} />
+
         <Route path="/attendance" element={<Attendance />} />
+
       </Routes>
-    </BrowserRouter>
+
+    </HashRouter>
+
   )
 }
 
